8c338f17bcfb65cd901abdca564432e26ac5824a
Shahzad Tariq <shahzad.tariq@nxb.com.pk>
2015-05-29 20:05:54 +0500

    desc for installer

--- lib/generators/trackstamps/install_generator.rb
+++ lib/generators/trackstamps/install_generator.rb
@@ -1,7 +1,7 @@
 module Trackstamps
   module Generators
     class InstallGenerator < Rails::Generators::Base
-      desc "Add MultiTenant functionality to OpenSourceBilling"
+      desc "Add Trackstamps functionality to Rails based web applications"
       source_root File.expand_path("../templates", __FILE__)
 
 
